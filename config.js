f5d93b891b3207359ef3d269eca0b41a216416c2
Aldrew M. Sevilla <51847569+S581@users.noreply.github.com>
2026-06-17 19:24:09 +0800

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.ENV = {
-  SUPABASE_URL:  'https://ucxxythaliocuqwvmgea.supabase.co',
-  SUPABASE_ANON: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjeHh5dGhhbGlvY3Vxd3ZtZ2VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2ODA1NzYsImV4cCI6MjA5NzI1NjU3Nn0.kb3iJqDuYnIeXeuxLiwDTJ97T9EX8ggdZHd0WWF35jQ',
-};
